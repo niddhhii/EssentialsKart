6d69917f72d29ead30f1f13f9b9f4ff24851e511
Kunal <kunal.bohra@somaiya.edu>
2020-06-04 13:04:03 +0530

    delete session added and updated actions

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import date
 import config
 from firebase import firebase
 import csv
+from twilio.rest import Client
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
@@ -39,18 +41,22 @@ def pushToDB():
     if curr_orders is not None:
         item_list = params['items']
         num_list = params['number']
-        order_dict = mkdict(curr_orders)
-        k = len(order_dict)
+        order_dict = curr_orders
         poslist = []
         neglist = []
         for i in range(len(item_list)):
             price = get_price(item_list[i])
-            if price != -1:
-                temp = [item_list[i], int(num_list[i]), price]
-                order_dict.update({i + k: temp})
-                poslist.append(item_list[i])
+            if item_list[i] not in order_dict:
+                if price != -1:
+                    temp = {item_list[i]: [int(num_list[i]), price]}
+                    order_dict.update(temp)
+                    poslist.append(item_list[i])
+                else:
+                    neglist.append(item_list[i])
             else:
-                neglist.append(item_list[i])
+                prev = order_dict[item_list[i]][0]
+                order_dict[item_list[i]][0] = prev + int(num_list[i])
+                poslist.append(item_list[i])
         db.child('orders').child(sess).update(order_dict)
     else:
         item_list = params['items']
@@ -61,8 +67,8 @@ def pushToDB():
         for i in range(len(item_list)):
             price = get_price(item_list[i])
             if price != -1:
-                temp = [item_list[i], int(num_list[i]), price]
-                order_dict.update({i: temp})
+                temp = {item_list[i]: [int(num_list[i]), price]}
+                order_dict.update(temp)
                 poslist.append(item_list[i])
             else:
                 neglist.append(item_list[i])
@@ -70,7 +76,7 @@ def pushToDB():
     text = ""
     if len(neglist) != 0:
         i = 0
-        text = "We don't sell "
+        text = "Sorry, we don't sell "
         for item in neglist:
             if i == (len(neglist) - 2):
                 text += item + " and "
@@ -79,6 +85,7 @@ def pushToDB():
             else:
                 text += item + ", "
             i += 1
+        text += "Please refer to the list we sent you earlier. "
         if len(poslist) != 0:
             text += "However, we have added "
             i = 0
@@ -94,15 +101,6 @@ def pushToDB():
     return text
 
 
-def mkdict(temp):
-    order_dict = {}
-    i = 0
-    for x in temp:
-        order_dict.update({i: x})
-        i += 1
-    return order_dict
-
-
 def get_price(item):
     with open('items.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -115,6 +113,47 @@ def get_price(item):
                 if row[0] == item:
                     return int(row[1])
         return -1
+
+
+def sendPDF(url):
+    client = Client(config.twilioSID, config.twilioAUTH)
+    from_whatsapp_number = '+14155238886'
+    to_whatsapp_number = '12'
+    message = client.messages.create(body='Hey! I am Natasha. Here to help you out in your order. Please check the '
+                                          'list below to find the essential items that you can order during lockdown'
+                                          'What would you like to order today?',
+                                     media_url=url,
+                                     from_=from_whatsapp_number,
+                                     to=to_whatsapp_number)
+    print(message.sid)
+
+
+def genPDF():
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template("invoice.html")
+    template_vars = {"title": "Sales Funnel Report - National",
+                     "national_pivot_table": ''}
+    html_out = template.render(template_vars)
+    # HTML(string=html_out).write_pdf("report.pdf")
+
+
+def del_sess():
+    req = request.get_json(force=True)
+    sess = req.get('session')[-36:] + "-" + str(date.today())
+    fb_app.delete('/orders', sess)
+
+
+def check_phone():
+    req = request.get_json(force=True)
+    phone = req.get('queryResult').get('parameters').get('contact')
+    ch_contact = fb_app.get('/users', phone)
+    if ch_contact is not None:
+        return "It seems we have already met."  # ask for otp
+    else:
+        sess = req.get('session')[-36:]
+        db = firebaseObj.database()
+        db.child('users').child(phone).set(sess)
+        return "Please enter your name now."
 
 
 @app.route('/webhook', methods=['POST'])
@@ -133,8 +172,9 @@ def webhook():
         }
         return make_response(jsonify(reply))
     elif action == 'OrderItems.OrderItems-cancel':  # delete the session and end it
+        del_sess()
         reply = {
-            "fulfillmentText": "Order Cancelled",
+            "fulfillmentText": "",
         }
         return make_response(jsonify(reply))
     elif action == 'confirm_details':  # ask for confirmation about order
@@ -145,6 +185,22 @@ def webhook():
     elif action == 'confirm_order':  # send final receipt over email and wapp
         reply = {
             "fulfillmentText": "Order confirmed",
+        }
+        return make_response(jsonify(reply))
+    elif action == 'edit_order':  # edit order
+        reply = {
+            "fulfillmentText": "Order edited",
+        }
+        return make_response(jsonify(reply))
+    elif action == 'edit_details':  # edit individual detail
+        reply = {
+            "fulfillmentText": "Edit Details",
+        }
+        return make_response(jsonify(reply))
+    elif action == 'check_number':  # check phone
+        text = check_phone()
+        reply = {
+            "fulfillmentText": text,
         }
         return make_response(jsonify(reply))
 
